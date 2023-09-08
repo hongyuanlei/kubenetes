59454a886377e844a543328f56cb10fa869cb7c2
hong.yuanlei <hong.yuanlei@rea-group.com>
2023-09-09 00:22:56 +0800

    install k8s v1.28

--- infrastructure/scripts/common.sh
+++ infrastructure/scripts/common.sh
@@ -25,7 +25,7 @@ sudo swapoff -a
 sudo apt-get update -y
 # Install CRI-O Runtime
 
-VERSION="$(echo ${KUBERNETES_VERSION} | grep -oE '[0-9]+\.[0-9]+')"
+#VERSION="$(echo ${KUBERNETES_VERSION} | grep -oE '[0-9]+\.[0-9]+')"
 
 # Create the .conf file to load the modules at bootup
 cat <<EOF | sudo tee /etc/modules-load.d/crio.conf
@@ -48,14 +48,16 @@ sudo sysctl --system
 cat <<EOF | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
 deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/ /
 EOF
-cat <<EOF | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$VERSION.list
-deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/ /
+cat <<EOF | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable:cri-o:$KUBERNETES_VERSION.list
+deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$KUBERNETES_VERSION/$OS/ /
 EOF
 
-curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$VERSION/$OS/Release.key \
-  | sudo apt-key --keyring /etc/apt/trusted.gpg.d/libcontainers.gpg add -
+curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable:/cri-o:/$KUBERNETES_VERSION/$OS/Release.key \
+  | gpg --dearmor \
+  | sudo tee /etc/apt/trusted.gpg.d/libcontainers.gpg
 curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/$OS/Release.key \
-  | sudo apt-key --keyring /etc/apt/trusted.gpg.d/libcontainers.gpg add -
+  | gpg --dearmor \
+  | sudo tee /etc/apt/trusted.gpg.d/libcontainers.gpg
 
 sudo apt-get update
 sudo apt-get install cri-o cri-o-runc -y
@@ -68,20 +70,29 @@ sudo systemctl enable crio --now
 
 echo "CRI runtime installed susccessfully"
 
+# install kubernetes & helm
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates curl
-sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg \
-  | sudo gpg --dearmor -o /usr/share/keyrings/kubernetes-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" \
+# kubernetes
+curl -fsSL "https://pkgs.k8s.io/core:/stable:/v${KUBERNETES_VERSION}/deb/Release.key" \
+  | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v${KUBERNETES_VERSION}/deb/ /" \
   | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+# helm
+sudo curl https://baltocdn.com/helm/signing.asc \
+  | gpg --dearmor \
+  | sudo tee /usr/share/keyrings/helm.gpg > /dev/null
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/helm.gpg] https://baltocdn.com/helm/stable/debian/ all main" \
   | sudo tee /etc/apt/sources.list.d/helm-stable-debian.list
+
 sudo apt-get update -y
 sudo apt-get install -y \
-  kubelet="$KUBERNETES_VERSION" \
-  kubectl="$KUBERNETES_VERSION" \
-  kubeadm="$KUBERNETES_VERSION" \
+  kubelet \
+  kubectl \
+  kubeadm \
   helm
+
 sudo apt-get update -y
 sudo apt-get install -y jq
 
